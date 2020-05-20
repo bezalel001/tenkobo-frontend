4d423417dc87466fe8b2bf6ab31733ed1c6c51f6
bezalel001 <mickeybezalel@gmail.com>
2020-05-20 11:02:02 +0100

    created project with create react and added files for homepage

---
+++ tenkobo/src/homepage.component.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+import './homepage.styles.scss';
+
+const Homepage = () => (
+	<div className='homepage'>
+		<div className='directory-menu'>
+			<div className='menu-item'>
+				<div className='content'>
+					<h1 className='title'>Oando Filling Station</h1>
+					<span className='subtitle'>shop now</span>
+				</div>
+			</div>
+			<div className='menu-item'>
+				<div className='content'>
+					<h1 className='title'>NNPC Filling Station</h1>
+					<span className='subtitle'>shop now</span>
+				</div>
+			</div>
+			<div className='menu-item'>
+				<div className='content'>
+					<h1 className='title'>TOTAL Filling Station</h1>
+					<span className='subtitle'>shop now</span>
+				</div>
+			</div>
+			<div className='menu-item'>
+				<div className='content'>
+					<h1 className='title'>Shell Filling Station</h1>
+					<span className='subtitle'>shop now</span>
+				</div>
+			</div>
+			<div className='menu-item'>
+				<div className='content'>
+					<h1 className='title'>Chevron Filling Station</h1>
+					<span className='subtitle'>shop now</span>
+				</div>
+			</div>
+			<div className='menu-item'>
+				<div className='content'>
+					<h1 className='title'>Mobil Filling Station</h1>
+					<span className='subtitle'>shop now</span>
+				</div>
+			</div>
+		</div>
+	</div>
+);
+
+export default Homepage;
